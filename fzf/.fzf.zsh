adb18827d02935caffffb0ed34e8da875c88d026
drover <drover@google.com>
2016-01-10 19:52:24 -0800

    Remove .fzf... that was a mistake

--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -5,25 +5,25 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # Custom fzf path
 # ---------------
-fzf_path=$HOME/github/dotfiles/fzf
+fzf_path=$HOME/.fzf
 
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *$fzf_path/.fzf/bin* ]]; then
-  export PATH="$PATH:$fzf_path/.fzf/bin"
+if [[ ! "$PATH" == *$fzf_path/bin* ]]; then
+  export PATH="$PATH:$fzf_path/bin"
 fi
 
 # Man path
 # --------
-if [[ ! "$MANPATH" == *$fzf_path/.fzf/man* && -d "$fzf_path/.fzf/man" ]]; then
-  export MANPATH="$MANPATH:$fzf_path/.fzf/man"
+if [[ ! "$MANPATH" == *$fzf_path/man* && -d "$fzf_path/man" ]]; then
+  export MANPATH="$MANPATH:$fzf_path/man"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$fzf_path/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$fzf_path/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "$fzf_path/.fzf/shell/key-bindings.zsh"
+source "$fzf_path/shell/key-bindings.zsh"
 
