6d8063d434438b561fee306241b7b81bf0e12e0d
coldis <m.drover25@gmail.com>
2018-10-10 11:05:16 -0700

    Add some stuff to setup

--- setup.zsh
+++ setup.zsh
@@ -1,6 +1,32 @@
-# Install antibody
+#-------------
+# Apt Packages
+#-------------
+sudo apt-get install fasd
+sudo apt-get install git
+sudo apt-get install git-extras
+sudo apt-get install neovim
+sudo apt-get install silversearcher-ag
+sudo apt-get install tmux
+
+#-----------------
+# Package Managers
+#-----------------
+
+# Vim Plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# ZSH Antibody
 sh -c "$(curl -sL git.io/antibody | sh -s)"
 
-# Install apt packages
-sudo apt-get install silversearcher-ag
+# TMUX Package Manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+#-----------------
+# Other packages
+#-----------------
+
+# FZF
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --bin
 
