2341dfa9df3e24e68e6b2c045dc2e89cee69ca98
Matt Drover <m.drover25@gmail.com>
2019-08-02 11:23:05 -0230

    Make iterm-profile work in tmux

--- .config/zsh/plugins/iterm.zsh
+++ .config/zsh/plugins/iterm.zsh
@@ -3,7 +3,6 @@ if [ "$TERM_PROGRAM" = 'iTerm.app' ]; then
   iterm-emit() {
     local template="\e]${1}\007"
     shift
-
     if [[ -n "$TMUX" || "$TERM" = tmux* ]]; then
       template="\ePtmux;\e${template}\e\\"
     fi
@@ -13,5 +12,9 @@ if [ "$TERM_PROGRAM" = 'iTerm.app' ]; then
   iterm-profile() {
     iterm-emit '1337;SetProfile=%s' "$1"
     export ITERM_PROFILE=$1
+    if [[ -n "$TMUX" || "$TERM" = tmux* ]]; then
+        tmux set-environment ITERM_PROFILE $1
+    fi
   }
+
 fi
