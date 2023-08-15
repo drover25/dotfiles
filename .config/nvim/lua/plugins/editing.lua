fb12c9cd9750e28b5f128dad0932da88a7566d1a
Matt Drover <m.drover25@gmail.com>
2023-08-15 16:24:50 -0230

    config dump

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -1,6 +1,6 @@
 return {
   { "editorconfig/editorconfig-vim", lazy = false },
-  { "tpope/vim-surround", lazy = false },
+  { "tpope/vim-surround", lazy = false, vscode = true },
   {
     "max397574/better-escape.nvim",
     lazy = false,
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,17 +1,4 @@
 return {
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      vim.list_extend(opts.sources, {
-        nls.builtins.code_actions.eslint_d,
-        nls.builtins.diagnostics.eslint_d,
-        nls.builtins.formatting.eslint_d,
-        nls.builtins.formatting.prettierd,
-      })
-    end,
-  },
-
   -- cmdline tools and lsp servers
   {
     "williamboman/mason.nvim",
